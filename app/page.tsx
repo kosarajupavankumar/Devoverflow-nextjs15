e10f547b84815243acbc91f4500f3bbfad7996e2
pavankumar <pavankumarkosaraju@gmail.com>
2024-11-05 22:11:48 +0530

    ESLint/Prettier configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-violet-600 font-black">
+    <h1 className="text-3xl font-black text-violet-600">
       Welcome to wolrd of Next.js
     </h1>
   );
