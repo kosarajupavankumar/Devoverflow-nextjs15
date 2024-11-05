83b9db3ec2c624d3295a9e71dc6ba5363c6784fc
kosaraju pavan kumar <63385894+kosarajupavankumar@users.noreply.github.com>
2024-11-05 10:50:29 -0700

    Update font styles and add duplicate heading in Home component (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-primary-500 ">
-      Welcome to wolrd of Next.js
-    </h1>
+    <>
+      <h1 className="text-3xl font-black text-primary-500 ">
+        Welcome to wolrd of Next.js
+      </h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to wolrd of Next.js
+      </h1>
+    </>
   );
 };
 
