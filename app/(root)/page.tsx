264827103d6b1b39caa55f3510df4b9a8f91a4ca
pavankumar <pavankumarkosaraju@gmail.com>
2024-11-06 22:45:14 +0530

    Add social authentication form and enhance auth layout; refactor home page and update VSCode settings

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-black text-primary-500 ">
+        Welcome to wolrd of Next.js
+      </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 };
 
-export default page;
+export default Home;
