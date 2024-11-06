16ede643a293ed9ff890fa9684183eab805576e4
kosaraju pavan kumar <63385894+kosarajupavankumar@users.noreply.github.com>
2024-11-05 21:24:06 -0700

    Add utility functions, update styles, and integrate dropdown menu component (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
 const Home = () => {
   return (
     <>
       <h1 className="text-3xl font-black text-primary-500 ">
         Welcome to wolrd of Next.js
       </h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to wolrd of Next.js
-      </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
