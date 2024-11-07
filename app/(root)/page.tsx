639236a2a3e387d8c7acf4c3f13ff3a97a63fc51
kosaraju pavan kumar <63385894+kosarajupavankumar@users.noreply.github.com>
2024-11-06 21:00:05 -0700

    Auth for GIthub (#11)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,26 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+import { auth, signOut } from '@/auth';
 
-const Home = () => {
+import ROUTES from '../constants/routes';
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <>
       <h1 className="text-3xl font-black text-primary-500 ">
         Welcome to wolrd of Next.js
       </h1>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          'use server';
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <button type="submit">Log out</button>
+      </form>
     </>
   );
 };
