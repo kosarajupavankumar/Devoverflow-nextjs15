c73554eabe1ff615fda1dc09592d3e7d547f9562
pavankumar <pavankumarkosaraju@gmail.com>
2024-11-07 21:20:06 +0530

    Add new pages for Tags, Jobs, Community, Profile, Collections, and Ask a Question; implement mobile navigation and sidebar links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,10 @@
-import { auth, signOut } from '@/auth';
-
-import ROUTES from '../constants/routes';
-
-const Home = async () => {
-  const session = await auth();
-  console.log(session);
+'use client';
+const Home = () => {
   return (
     <>
       <h1 className="text-3xl font-black text-primary-500 ">
         Welcome to wolrd of Next.js
       </h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          'use server';
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <button type="submit">Log out</button>
-      </form>
     </>
   );
 };
