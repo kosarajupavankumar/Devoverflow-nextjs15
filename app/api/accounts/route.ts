c4ed66917111aaf6c2de312837a8013b97524c87
pavankumar <pavankumarkosaraju@gmail.com>
2024-11-22 18:46:04 +0530

    Remove unused ValidationError import from account management API

--- app/api/accounts/route.ts
+++ app/api/accounts/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 import Account from '@/database/account.model';
 import handleError from '@/lib/handlers/error';
-import { ForbiddenError, ValidationError } from '@/lib/http-errors';
+import { ForbiddenError } from '@/lib/http-errors';
 import dbConnect from '@/lib/mongoose';
 import { AccountSchema } from '@/lib/validation';
 import { APIErrorResponse } from '@/types/global';
