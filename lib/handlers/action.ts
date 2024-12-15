4ba06a334dce4aeb3ea525be216b76a618b95e28
pavankumar <pavankumarkosaraju@gmail.com>
2024-12-15 14:09:55 +0530

    Implement action handler with schema validation, authorization check, and database connection

---
+++ lib/handlers/action.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { Session } from 'next-auth';
+import { ZodError, ZodSchema } from 'zod';
+
+import { auth } from '@/auth';
+
+import { UnauthorizedError, ValidationError } from '../http-errors';
+import dbConnect from '../mongoose';
+
+type ActionOptions<T> = {
+  params?: T;
+  schema?: ZodSchema<T>;
+  authorize?: boolean;
+};
+
+// 1. checking whether the schema is provided and the params are passed and validated
+// 2. checking whether the user is authorized
+// 3. connecting to the database
+// 4. returning the session and params
+async function action<T>({ params, schema, authorize }: ActionOptions<T>) {
+  if (schema && params) {
+    try {
+      schema.parse(params);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<string, string[]>,
+        );
+      } else {
+        return new Error('Schema validation failed');
+      }
+    }
+  }
+
+  let session: Session | null = null;
+
+  if (authorize) {
+    session = await auth();
+    if (!session) {
+      return new UnauthorizedError();
+    }
+  }
+
+  await dbConnect();
+
+  return { session, params };
+}
+
+export default action;
